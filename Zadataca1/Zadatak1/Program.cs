26cb2b7189e52e18dc249b6e8e0eccfcb4d6fc1b
miaoreskovic <mia.oreskovic92@gmail.com>
2017-10-22 22:35:55 +0200

    dodana rjesenja za prvi i drugi zadatak

---
+++ Zadataca1/Zadatak1/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak1
+{
+
+    public class IntegerList : IIntegerList
+    {
+        private int[] _internalStorage;
+
+        //private int[] internalStorage;
+        int lastIndex;
+
+        public IntegerList()
+        {
+            _internalStorage = new int [3];
+            lastIndex = 0;
+        }
+
+        public IntegerList(int initialSize)
+        {
+            if (initialSize >= 0) _internalStorage = new int [initialSize];
+            else Console.WriteLine("Error occured, input must be greater than -1");
+            lastIndex = 0;
+        }
+
+
+
+
+        public void Add(int item)
+        {
+            Console.WriteLine(lastIndex);
+
+        }
+
+        public bool Remove(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetElement(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(int item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IIntegerList
+    {
+        /// <summary >
+        /// Adds an item to the collection .
+        /// </ summary>
+        void Add(int item);
+
+        /// <summary >
+        /// Removes the first occurrence of an item from the collection .
+        /// If the item was not found , method does nothing and returns false .
+        /// </ summary >
+        bool Remove(int item);
+
+        /// <summary >
+        /// Removes the item at the given index in the collection .
+        /// Throws IndexOutOfRange exception if index out of range .
+        /// </ summary >
+        bool RemoveAt(int index);
+
+        /// <summary >
+        /// Returns the item at the given index in the collection .
+        /// Throws IndexOutOfRange exception if index out of range .
+        /// </ summary >
+        int GetElement(int index);
+
+        /// <summary >
+        /// Returns the index of the item in the collection .
+        /// If item is not found in the collection , method returns -1.
+        /// </ summary >
+        int IndexOf(int item);
+
+        /// <summary >
+        /// Readonly property . Gets the number of items contained in the
+        ///collection.
+        /// </ summary >
+        int Count { get; }
+
+        /// <summary >
+        /// Removes all items from the collection .
+        /// </ summary >
+        void Clear();
+
+        /// <summary >
+        /// Determines whether the collection contains a specific value .
+        /// </ summary >
+        bool Contains(int item);
+    }
+    
+}
---
+++ Zadataca1/Zadatak2/GenericList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak2
+{
+    public class GenericList<X> : IGenericList<X>
+    {
+        private X[] _internalStorage;
+        private int _lastIndex;
+        private int size;
+        private int _removeFromIndex;
+
+        public GenericList()
+        {
+            _internalStorage = new X[3];
+            _lastIndex = -1;
+            size = 3;
+        }
+
+        public GenericList(int initialSize)
+        {
+            _internalStorage = new X[initialSize];
+            _lastIndex = -1;
+            size = initialSize;
+        }
+
+        public void Add(X item)
+        {
+            X[] internalStorage = new X[size];
+            internalStorage = _internalStorage;
+            //int lastIndex = _lastIndex;
+            if ((_lastIndex + 1) == size)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    internalStorage[i] = _internalStorage[i];
+                }
+                _internalStorage = new X[2 * size];
+                for (int i = 0; i < size; i++)
+                {
+                    _internalStorage[i] = internalStorage[i];
+                }
+                //internalStorage = _internalStorage;
+            }
+            _internalStorage[_lastIndex + 1] = item;
+            _lastIndex++;
+        }
+
+        public bool Remove(X item)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (_internalStorage[i].Equals(item))
+                {
+                    _removeFromIndex = i;
+                    return RemoveAt(_removeFromIndex);
+                }
+            }
+            return false;
+        }
+
+        public bool RemoveAt(int index)
+        {
+            int[] internalStorage = new int[size];
+            X temp;
+            if (index > size) throw new IndexOutOfRangeException();
+            //int j = 0;
+            for (int i = index; i + 1 < size; i++)
+            {
+                temp = _internalStorage[i + 1];
+                _internalStorage[i] = temp;
+            }
+            //_internalStorage = internalStorage;
+            _lastIndex--;
+            return true;
+        }
+
+        public X GetElement(int index)
+        {
+            if (index <= size) return _internalStorage[index];
+            throw new IndexOutOfRangeException();
+        }
+
+        public int IndexOf(X item)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (_internalStorage[i].Equals(item)) return i;
+            }
+            return -1;
+        }
+
+
+        public int Count { get => _lastIndex + 1; }
+        public void Clear()
+        {
+            _lastIndex = -1;
+        }
+
+        public bool Contains(X item)
+        {
+            //int[] internalStorage = _internalStorage;
+            if (_lastIndex == -1) return false;
+            for (int i = 0; i < size; i++)
+            {
+                if (_internalStorage[i].Equals(item)) return true;
+            }
+            return false;
+        }
+    }
+}
